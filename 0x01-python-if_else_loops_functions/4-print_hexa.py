e65f49a7442b10ca3cfd88c066190012011559f2
Saad Karkal <karkalsaad@gmail.com>
2023-06-11 11:21:10 +0100

    done

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,7 +2,7 @@
 
 def print_hex(i):
     hex_value = hex(i)[2:]
-    print(i, end=" = 0x" + hex_value + "\n")
+    print("{} = 0x{}".format(i, hex_value))
 
 for i in range(0, 99):
     print_hex(i)
