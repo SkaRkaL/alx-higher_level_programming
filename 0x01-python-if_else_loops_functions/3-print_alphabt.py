a7d97dd5aa6a5e03bd044cc68b5e901ecf6e2661
Saad Karkal <karkalsaad@gmail.com>
2023-06-11 10:58:19 +0100

    done

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for ascii_value in range(97, 123):
+    character = chr(ascii_value)
+    if character == 'e' or character == 'q':
+        continue
+    print(character, end="")
