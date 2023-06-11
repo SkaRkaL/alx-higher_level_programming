c5aa4e0fce0cbd3c9931b7f060c6977672496089
Saad Karkal <karkalsaad@gmail.com>
2023-06-11 23:39:42 +0100

    done

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-def ft_putchar(c):
-    print(c, end='')
-
-def ft_print_comb():
-    b = 0
-    while b <= 98:
-        ft_putchar(chr(ord('0') + b // 10))
-        ft_putchar(chr(ord('0') + b % 10))
-        if b != 98:
-            ft_putchar(',')
-            ft_putchar(' ')
-        b += 1
-
-ft_print_comb()
-print()
+#!/usr/bin/python3
+for nbr in range(0, 100):
+    if nbr == 99:
+        print("{}".format(nbr))
+    else:
+        print("{:02}".format(nbr), end=", ")
